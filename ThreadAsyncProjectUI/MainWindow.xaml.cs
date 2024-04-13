b3710888f1902806ae7176f4e4eb3f145e6dfd78
Snizhana Kostiuk <snizhana2812kostyuk@gmail.com>
2024-04-13 23:14:13 +0300

    done task 1

--- ThreadAsyncProjectUI/MainWindow.xaml.cs
+++ ThreadAsyncProjectUI/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ThreadAsyncProjectUI
 {
@@ -16,66 +8,33 @@ namespace ThreadAsyncProjectUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int number;
-        private int milliseconds;
-        private bool stop;
+        private bool start;
+        private readonly Random rnd = new Random();
         public MainWindow()
         {
             InitializeComponent();
+            start = false;
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            //Thread thread = new Thread(ShowMessage);
-            //thread.Start();
-            stop = !stop;
-        }
-
-        private async Task StartTimer()
-        {
-            while (true)
+            if (e.Key == Key.S)
             {
+                start = !start;
+                if (start)
+                {
+                    _ = GenerateNumbersAsync();
+                }
+            }
+        }
+
+        private async Task GenerateNumbersAsync()
+        {
+            while (start)
+            {
+                await Dispatcher.InvokeAsync(() => textBlock.Text = rnd.Next().ToString());
                 await Task.Delay(1000);
-                if (stop) continue;
-                number++;
-                textBlock.Text = number.ToString();
             }
         }
-
-        private async Task StartTimer2()
-        {
-            while (true)
-            {
-                await Task.Delay(10);
-                if (stop) continue;
-                milliseconds++;
-                textBlock2.Text = milliseconds.ToString();
-                if (milliseconds == 100) milliseconds = 0;
-            }
-        }
-
-        async Task ShowMessageAsync()
-        {
-            await Task.Delay(1000);
-            button.Content = "Thread works";
-        }
-
-        private async void startButton_Click(object sender, RoutedEventArgs e)
-        {
-            StartTimer();
-            await StartTimer2();
-        }
-
-        //void ShowMessage()
-        //{
-        //    Thread.Sleep(5000);
-        //    Dispatcher.Invoke(() =>
-        //    {
-        //        button.Content = "Thread works";
-        //    });
-        //}
-
-
-        //
     }
 }
